317229d5bb959fd54c791353b76d2963a50e7d6f
AbnerMJA <abnerpaz.gomes@outlook.com>
2025-07-14 19:45:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const App = () => {
 
   const handleAdd = () => {
     const novaEntrada = { ...formData, id: uuidv4() };
-    const existentes = Array.isArray(appointments[dateKey]) ? appointintments[dateKey] : [];
+    const existentes = appointments[dateKey] || [];
     if (!fromData.horario || !formData.duracao) {
       alert("Preencha horário e duração antes de agendar.");
       return;
